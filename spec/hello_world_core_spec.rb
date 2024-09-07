e0ecf9865d7449dd99afdd803453a5a399a558c0
Adam Bonsu <adam@bonsu.io>
2024-09-07 15:26:30 +0100

    add unit test

---
+++ spec/hello_world_core_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative '../lib/hello_world_hex_ruby/core/hello_world_core'
+
+module HelloWorldHexRuby
+    module Core
+        RSpec.describe HelloWorldCore do
+            it 'says hello' do
+                user_interface = double
+                core = HelloWorldCore.new(user_interface)
+                expect(user_interface).to receive(:display).with('Hello, Ruby!')
+                core.say_hello('Ruby')
+            end
+        end
+    end
+end
