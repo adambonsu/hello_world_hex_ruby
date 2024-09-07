41622bea4a5bbe5b796527721e8576a74193dd96
Adam Bonsu <adam@bonsu.io>
2024-09-07 15:30:16 +0100

    linting

--- spec/hello_world_core_spec.rb
+++ spec/hello_world_core_spec.rb
@@ -3,14 +3,14 @@
 require_relative '../lib/hello_world_hex_ruby/core/hello_world_core'
 
 module HelloWorldHexRuby
-    module Core
-        RSpec.describe HelloWorldCore do
-            it 'says hello' do
-                user_interface = double
-                core = HelloWorldCore.new(user_interface)
-                expect(user_interface).to receive(:display).with('Hello, Ruby!')
-                core.say_hello('Ruby')
-            end
-        end
+  module Core
+    RSpec.describe HelloWorldCore do
+      it 'says hello' do
+        user_interface = double
+        core = HelloWorldCore.new(user_interface)
+        expect(user_interface).to receive(:display).with('Hello, Ruby!')
+        core.say_hello('Ruby')
+      end
     end
+  end
 end
