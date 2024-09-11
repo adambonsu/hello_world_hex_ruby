38df7026efbdb3ec8579a466e6608589f15fc6b1
Adam Bonsu <adam@bonsu.io>
2024-09-11 21:38:44 +0100

    linting

--- spec/ports/user_interface_spec.rb
+++ spec/ports/user_interface_spec.rb
@@ -5,12 +5,12 @@ require_relative '../../lib/hello_world_hex_ruby/ports/user_interface'
 module HelloWorldHexRuby
   module Ports
     RSpec.describe UserInterface do
-        context '#display' do
-          it 'raises a NotImplementedError' do
-            user_interface = UserInterface.new
-            expect { user_interface.display(nil) }.to raise_error(NotImplementedError)
-          end
+      context '#display' do
+        it 'raises a NotImplementedError' do
+          user_interface = UserInterface.new
+          expect { user_interface.display(nil) }.to raise_error(NotImplementedError)
         end
+      end
     end
   end
 end
