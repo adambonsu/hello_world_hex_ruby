8f3f6f7377e408e9037dec5d05efa6dfa2d4dc96
Adam Bonsu <adam@bonsu.io>
2024-09-13 10:14:54 +0100

    linting

--- lib/hello_world_hex_ruby.rb
+++ lib/hello_world_hex_ruby.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
+
 require 'require_all'
 require_all 'lib/hello_world_hex_ruby/core/**/*.rb'
 require_all 'lib/hello_world_hex_ruby/ports/**/*.rb'
 require_all 'lib/hello_world_hex_ruby/adaptors/**/*.rb'
 require_relative 'hello_world_hex_ruby/version'
-
 
 module HelloWorldHexRuby
   class Error < StandardError; end
